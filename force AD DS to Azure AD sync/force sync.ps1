f03770de3eef008bdee429e7b875e486a4bdfdb3
madsamuel <stgeorgi@microsoft.com>
2020-10-28 11:50:06 -0700

    Create force sync.ps1

---
+++ force AD DS to Azure AD sync/force sync.ps1
@@ -0,0 +1,13 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Find-PackageProvider -Name "Nuget" -AllVersions
+
+Install-PackageProvider -Name "Nuget"
+
+Import-Module –Name "C:\Program Files\Microsoft Azure AD Sync\Bin\ADSync" -Verbose
+
+get-module
+
+get-command -module adsync
+
+Start-ADSyncSyncCycle -PolicyType Delta
